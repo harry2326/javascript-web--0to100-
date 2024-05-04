42918b903163fc8dbd458ff7c6c623f1581caf3c
harry2326 <94780140+harry2326@users.noreply.github.com>
2024-05-04 13:50:36 +0000

    Lecture 2nd (basics)

---
+++ 02basics/2nd.js
@@ -0,0 +1,11 @@
+// use strcit means that we are commiting to use only the mordern advance keywords or methods of writing javascript
+// data types in javascript numberr, string ,boolean , bigint, null, undefined, symbol, obj, 
+"use strict";
+
+let a=1;
+let b= "Abcd";
+let c= true;
+let d=BigInt(1222321311243124254325345345345345345242142345345453);
+let e;
+let f=null;
+console.log([a,b,c,d,e,f]);
