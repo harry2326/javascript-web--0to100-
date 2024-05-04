8ad493eebbff34eb3f51b2a90191e4132313eb86
harry2326 <94780140+harry2326@users.noreply.github.com>
2024-05-04 13:42:59 +0000

    this is committed

---
+++ 01basics/1stprogram.js
@@ -0,0 +1,12 @@
+// VAR value assigned to var can be changed and 2nd issue with var is that it dosent have function scope or isnt a scope based 
+// LET value assigned to var can also be changed but it is a scoped based || blockbased 
+// CONST its value cannot be changed and is block based
+
+console.log("harry");
+const nam="harry";
+const age=22;
+let interests="robotics";
+var crorepati = "banuga eik din";
+
+crorepati ="bngya ajj";
+console.log([nam,age,interests,crorepati]);
