19b43f6129e1cc28c12c4f758b27b0777d2e19a7
harry2326 <94780140+harry2326@users.noreply.github.com>
2024-05-04 14:52:57 +0000

    03 - DATA CONVERSION

---
+++ 03basics/3rd.js
@@ -0,0 +1,14 @@
+//  talking about conversion of data from one type to another type 
+let name="harry";
+let CDname= Boolean(name);
+console.log(CDname);
+ 
+let age=22;
+let CDage=String(age);
+console.log( typeof CDage);
+
+let a=0;
+let b=1;
+let c= Boolean(a);
+let d= Boolean(b);
+console.log([typeof c, typeof d,c,d]);
